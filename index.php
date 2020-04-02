65d99525bfea314041379c507a6b66388d03e1ac
Michele Oliveira <michelemello1@gmail.com>
2020-04-02 20:14:41 -0300

    resolvi os comflitos de olas2

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
 
-<<<<<<< HEAD
 echo "Olá Mundo 456";
-=======
-echo "Olá Mundo 123";
 
-echo "Olá Michele";
->>>>>>> 82828b2ff07770c41d10d453e582f37d2f11025e
 
-echo "Olá udeny"
+echo "Olá Udemy e Hcode";
+
 ?>
